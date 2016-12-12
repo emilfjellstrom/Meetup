b8ed5207286fa29707ab172fc79d33c868a64ad6
Robert Johansson <rjn@l.ohb-sweden.se>
2016-12-12 19:11:14 +0100

    First

---
+++ MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Meetup
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+        }
+
+        private async void getDataClicked(object sender, RoutedEventArgs e)
+        {
+            var client = new HttpClient();
+            var result = await client.GetAsync("https://api.meetup.com/recommended/events?&signed=true&key=6f5a6d541d54b172e77721e2327e5e");
+
+            outputLabel.Text = await result.Content.ReadAsStringAsync();
+        }
+    }
+}
